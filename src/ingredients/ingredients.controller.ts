68e37de36899252945d3fc0ac450ef6237a26d1e
Myltiane Alves <myltiane.alves@cleandev.com.br>
2022-03-07 19:57:57 -0300

    Table ingredients_types

---
+++ src/ingredients/ingredients.controller.ts
@@ -0,0 +1,10 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { IngredientsService } from './ingredients.service';
+
+@Controller("ingredients")
+export class IngredientsController {
+    constructor(private ingredientService: IngredientsService) {}
+
+    @Post
+    create(@Body() )
+}
---
+++ src/ingredients/ingredients.module.ts
@@ -0,0 +1,10 @@
+import { IngredientsService } from './ingredients.service';
+import { Module } from '@nestjs/common';
+
+@Module({
+    imports: [],
+    controllers: [],
+    providers: [
+        IngredientsService,],
+})
+export class IngredientsModule { }
