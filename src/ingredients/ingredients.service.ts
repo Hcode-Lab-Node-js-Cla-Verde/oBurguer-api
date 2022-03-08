a221288649a96b3c4008faa5b3d47c7ee027c073
Myltiane Alves <myltiane.alves@cleandev.com.br>
2022-03-07 22:27:50 -0300

    created ingredients migration

--- src/ingredients/ingredients.service.ts
+++ src/ingredients/ingredients.service.ts
@@ -7,5 +7,16 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class IngredientsService {
 
+    async findAll() {}
+
+    async findOne(id: number) {}
+
+    async create() {}
+
+    async update() {}
+
+    async delete(id: number) {}
     
- }
+}
+
+ 
--- typeorm/migration/1646354789855-ingredients.ts
+++ typeorm/migration/1646354789855-ingredients.ts
@@ -1,10 +1,24 @@
-
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { MigrationInterface, QueryRunner, Table, } from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableForeignKey, } from "typeorm";
 
 export class ingredients1646354789855 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "ingredient_types",
+            columns: [{
+                name: "id",
+                type: "int",
+                isPrimary: true,
+                isGenerated: true,
+                generationStrategy: "increment"
+            }, {
+                name: "name",
+                type: "varchar",
+                length: "250",
+                isNullable: false,
+            }]
+        }));
+
         await queryRunner.createTable(new Table({
             name: "ingredients",
             columns: [{
@@ -24,10 +38,9 @@ export class ingredients1646354789855 implements MigrationInterface {
                 precision: 10,
                 scale: 2,
                 default: 0,
-                isUnique: true,
             }, {
-                name: "type",
-                type: "tinyint",
+                name: 'type_id',
+                type: 'int',
                 isNullable: false,
             }, {
                 name: "available",
@@ -44,11 +57,72 @@ export class ingredients1646354789855 implements MigrationInterface {
             }]
 
         }));
+
+        await queryRunner.createForeignKey('ingredients', new TableForeignKey({
+            columnNames: ['type_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'ingredient_types',
+            name: 'FK_ingredients_ingredients_types',
+            onDelete: 'CASCADE',
+        }));
+
+        const ingredientTypes = ['Padrão', 'Pão'];
+
+        for (let index = 0; index < ingredientTypes.length; index++) {
+            await queryRunner.query(`INSERT INTO  ingredient_types(name) VALUES ('${ingredientTypes[index]}');`);
+        }
+
+        const ingredients = [
+          {
+            name: 'Pão Tradicional',
+            price: 2.00,
+            typeId: 2,
+          },
+          {
+            name: 'Pão de Batata',
+            price: 3.00,
+            typeId: 2,
+          },
+          {
+            name: 'Pão italiano',
+            price: 5.00,
+            typeId: 2,
+          },
+          {
+            name: 'Queijo Mussarela',
+            price: 4.00,
+            typeId: 1,
+          },
+          {
+            name:  'Tomate',
+            price: 5.00,
+            typeId: 1,
+          },
+          {
+            name: 'Carne de Frango 130g',
+            price: 4.00,
+            typeId: 1,
+          },
+          {
+            name: 'Alface',
+            price: 4.00,
+            typeId: 1,
+          },
+        ];
+
+        for (let index = 0; index < ingredients.length; index++) {
+            const { name, price, typeId } = ingredients[index];
+
+            await queryRunner.query(`INSERT INTO ingredients(name, price, type_id, available) VALUES ('${name}', '${price}', ${typeId}, 1);`);
+        }
+
     }
 
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey("ingredients", "FK_ingredients_ingredients_types")
         await queryRunner.dropTable("ingredients")
+        await queryRunner.dropTable("ingredient_types")
     }
 
 }
