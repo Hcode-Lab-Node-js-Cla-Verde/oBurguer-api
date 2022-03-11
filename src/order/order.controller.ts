af6e50a4ab1d3bcb2f926cd7e205c79fd2384844
Pedro Henrique <phfs28@gmail.com>
2022-03-10 21:36:59 -0300

    Added creteOrder service

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { OrderService } from './order.service';
-import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 
 @Controller('orders')
 export class OrderController {
@@ -18,13 +16,8 @@ export class OrderController {
   }
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.orderService.create(createOrderDto);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.orderService.update(+id, updateOrderDto);
+  create(@Body() body) {
+    return this.orderService.create(body);
   }
 
   @Delete(':id')
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,13 +1,16 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateOrderDto } from './dto/create-order.dto';
+import { UserService } from 'src/user/user.service';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { isValidNumber } from 'src/utils';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private userService: UserService,
+  ) {}
 
   findAll() {
     return this.prismaService.order.findMany({
@@ -36,10 +39,51 @@ export class OrderService {
     return order;
   }
 
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
-  }
+  async create({ userId, items }) {
+    userId = isValidNumber(userId);
 
+    if (!items.length) {
+      throw new BadRequestException('Order must have at least one item');
+    }
+
+    items.forEach(item => {
+      if (!item.ingredients.length) {
+        throw new BadRequestException('Item must have at least one ingredient');
+      }
+    });
+
+    // Checks if user exists
+    await this.userService.findById(userId);
+
+    const order = await this.prismaService.order.create({
+      data: {
+        statusId: 1,
+        userId,
+        total: 10.2, // TODO: Count order total
+      },
+    });
+
+    for (const item of items) {
+      const orderItem = await this.prismaService.orderItem.create({
+        data: {
+          orderId: order.id,
+          name: item.name,
+        },
+      });
+
+      for (const ingredientId of item.ingredients) {
+        await this.prismaService.itemIngredient.create({
+          data: {
+            ingredientId,
+            orderItemId: orderItem.id,
+            ingredient_price: 10.00,
+          },
+        })
+      }
+    }
+
+    return this.findOne(order.id);
+  }
 
   update(id: number, updateOrderDto: UpdateOrderDto) {
     return `This action updates a #${id} order`;
