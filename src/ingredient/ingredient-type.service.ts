1195d99b386239445d6e5e2048b2928c38a7274a
Pedro Henrique <phfs28@gmail.com>
2022-03-16 20:42:50 -0300

    Removed unused methods

--- src/ingredient/ingredient-type.service.ts
+++ src/ingredient/ingredient-type.service.ts
@@ -1,21 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { isValidNumber } from 'src/utils/';
-import { CreateIngredientDto } from './dto/create-ingredient.dto';
 
 @Injectable()
 export class IngredientTypeService {
 
-  constructor(private prismaService: PrismaService) {} 
-
-  create(id: number, { name }: CreateIngredientDto) {
-    return this.prismaService.ingredientType.create({
-      data: {
-        name: name,
-        id: isValidNumber(id)
-      },
-    });
-  }
+  constructor(private prismaService: PrismaService) {}
 
   findAll() {
     return this.prismaService.ingredientType.findMany();
@@ -23,14 +13,6 @@ export class IngredientTypeService {
 
   findOne(id: number) {
     return this.prismaService.ingredientType.findUnique({
-      where: {
-        id: isValidNumber(id),
-      },
-    });
-  }
-
-  remove(id: number) {
-    return this.prismaService.ingredientType.delete({
       where: {
         id: isValidNumber(id),
       },
