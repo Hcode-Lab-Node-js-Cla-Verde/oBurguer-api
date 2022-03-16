dd9896fbf674a236ea4976f1a708a9794810d8f9
Pedro Henrique <phfs28@gmail.com>
2022-03-16 20:43:55 -0300

    Added Auth guard

--- src/ingredient/ingredient.controller.ts
+++ src/ingredient/ingredient.controller.ts
@@ -1,29 +1,31 @@
-import { Body, Controller, Post, Get, Delete, Param } from '@nestjs/common';
+import { Body, Controller, Post, Get, Delete, Param, UseGuards } from '@nestjs/common';
 import { IngredientService } from './ingredient.service';
 import { CreateIngredientDto } from './dto/create-ingredient.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('ingredients')
 export class IngredientController {
   constructor(private ingredientService: IngredientService) {}
 
+  @UseGuards(AuthGuard)
   @Get()
   async list() {
     return this.ingredientService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id) {
     return this.ingredientService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard)
   @Post()
-  async create(
-    @Body() data: CreateIngredientDto,
-    @Body() user,
-  ) {
-    return this.ingredientService.create(user.type_id, data);
+  async create( @Body() data: CreateIngredientDto) {
+    return this.ingredientService.create(data);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async remove(@Param('id') id) {
     return this.ingredientService.remove(+id);
