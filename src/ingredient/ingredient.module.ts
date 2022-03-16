e5cd116b036919743000cf5ef223f3525e0c38b8
Pedro Henrique <phfs28@gmail.com>
2022-03-16 20:34:18 -0300

    Added Auth and User modules

--- src/ingredient/ingredient.module.ts
+++ src/ingredient/ingredient.module.ts
@@ -3,10 +3,14 @@ import { Module } from '@nestjs/common';
 import { IngredientController } from './ingredient.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { IngredientTypeService } from './ingredient-type.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     PrismaModule,
+    AuthModule,
+    UserModule
   ],
   controllers: [
     IngredientController,
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -5,6 +5,7 @@ import { OrderController } from './order.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserModule } from 'src/user/user.module';
 import { AuthModule } from 'src/auth/auth.module';
+
 @Module({
   imports: [PrismaModule, UserModule, AuthModule],
   controllers: [OrderController],
